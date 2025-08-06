266e855695986790b3dfea1a80fc4c41ce824aca
Matthew Cadena <mcadena@andrew.cmu.edu>
2025-08-05 22:31:55 -0700

    Initial Commit

---
+++ SwimmingFish/SwimmingFishApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwimmingFishApp.swift
+//  SwimmingFish
+//
+//  Created by Matt Cadena on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwimmingFishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
